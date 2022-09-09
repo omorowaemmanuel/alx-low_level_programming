fe5c4fb3bbff3454edb3ee1f6865dcef2d0bdafc
omorowaemmanuel <omorowaemmanuel@gmail.com>
2022-09-08 17:56:52 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
+ *
+ * Description: prints and that piece..
  * Return: Always 1 (Success)
  */
 int main(void)
